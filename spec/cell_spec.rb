474f9ba6b1fbdaca485faed77ee97413e11c0e1f
Garrett Simpson <elgaricimo@gmail.com>
2014-11-15 16:24:33 -0500

    Added spec for Cell class

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,10 +1,21 @@
-require_relative "./spec_helper.rb"
+#require_relative "./spec_helper.rb"
+require_relative "spec_helper"
+
 
 module SmartTacToe
-  describe Cell, "#instantiate" do
-    it "is an empty string if instantiated with no argument" do
-      square = Cell.new
-      expect(square).to eq ""
+  describe Cell, "#initialize" do
+    it "is an empty string if initialized with no argument" do
+      empty_square = Cell.new
+      square_value = empty_square.value
+      expect(square_value).to eq ""
     end
+
+    it "has the value of the passed in argument if initialized with an argument" do
+      move = "X"
+      filled_square = Cell.new(move)
+      square_value = filled_square.value
+
+      expect(square_value).to eq move
+    end 
   end
 end
