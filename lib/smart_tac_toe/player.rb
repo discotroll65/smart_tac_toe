ccff882ad312d159fa9e6fa508211cfbbf657bbf
Garrett Simpson <elgaricimo@gmail.com>
2014-11-17 23:24:45 -0500

    Can see if Player#about_to_win?

--- lib/smart_tac_toe/player.rb
+++ lib/smart_tac_toe/player.rb
@@ -1,19 +1,35 @@
+require_relative "./strategic.rb"
+
 module SmartTacToe
   class Player
+    include Strategic
     attr_accessor :name, :marker
+
     def initialize (marker="X")
       @name = "Human"
       @marker = marker
     end
 
+    def about_to_win?(board)
+      result = false
+      win_combos = board.win_combos
 
-    def possible_wins(board)
-      available_winning_combos.each do |possible_win|
-        possible_win.each do |move|
-
-        end
-
+      win_combos.each do |combo|
+        marks = self.count_marks(combo)
+        result = true if marks == 2 && !combo_dead?(combo)
       end
+
+      result
+    end
+
+    def count_marks(combo)
+      count = 0
+      marked = self.marker
+
+      combo.each do |cell|
+        count += 1 if cell.value == marked
+      end
+      count
     end
 
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,6 +8,33 @@ module SmartTacToe
     end
   end
 
+  describe Player, "#about_to_win?" do
+    board = Board.new
+    player = Player.new("X")
+    it "returns true if a player has an imminent win" do
+      board.fill_cell(0,0,"X")
+      board.fill_cell(0,1,"X")
+      expect(player.about_to_win?(board)).to eq true
+    end
+    it "returns false if a player does not have an imminent win" do
+      board.fill_cell(0,2,"O")
+      expect(player.about_to_win?(board)).to eq false
+    end
+  end
+
+  describe Player, "#count_marks(combo)" do
+    it "counts how many of that player's marks are in an available win combo" do
+      board = Board.new
+      player = Player.new("X")
+      board.fill_cell(0,0,"X")
+      board.fill_cell(0,1,"X")
+      combo = board.win_combos[0]
+      count = player.count_marks(combo)
+      expect(count).to eq 2
+    end
+
+  end
+
 
 
 end
